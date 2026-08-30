55fefde87f99ece1218d075a3a47111f93f8111d
cy26015 <cy26015@shibaura-it.ac.jp>
2026-08-30 11:50:06 +0900

    鍵盤を並べた

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,9 +1,30 @@
 function Keyboard() {
+    const notes: string[] = [
+        "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4",
+        "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5"
+    ];
+
+    const keys = notes.map((note, index) => {
+        const type = note.includes("#") ? "black" : "white";
+
+        return {
+            note: note,
+            index: index,
+            type: type
+        };
+    })
+
     return (
         <div>
-          Keyboardコンポーネント
+            {keys.map((key) => {
+                return (
+                    <div key={key.index}>
+                        {key.note} - {key.type}
+                    </div>
+                );
+            })}
         </div>
-    )
+    );
 }
 
-export default Keyboard
+export default Keyboard;
