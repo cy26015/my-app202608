e7255baea38739481c3180532d879ca9e1486262
cy26015 <cy26015@shibaura-it.ac.jp>
2026-09-01 16:30:45 +0900

    highlight

--- src/components/HeaderQuiz.tsx
+++ src/components/HeaderQuiz.tsx
@@ -5,16 +5,36 @@ import Keyboard from "./Keyboard";
 "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A6", "A#6", "B5"
 */
 
-const majorTriads: Record<string, string>[] => {
-    C: ["C4", "E4", "G4"]
-    D: ["D4", "F#4", "A5"]
-    E: ["E4", "G4", "B5"]
-    F: ["F4", "A5", "C5"]
-    G: ["G4", "B5", "D5"]
-    A: ["A5", "C#5", "E5"]
+const MajorTriads: Record<string,string[]> = {
+    C: ["C4", "E4", "G4"],
+    D: ["D4", "F#4", "A5"],
+    E: ["E4", "G4", "B5"],
+    F: ["F4", "A5", "C5"],
+    G: ["G4", "B5", "D5"],
+    A: ["A5", "C#5", "E5"],
     B: ["B5", "D5", "F5"]
+};
+
+function HeaderQuiz() {
+    const currentRoot = "C";
+
+    const highlightKeys = MajorTriad[currentRooot];
+    
+    return (
+        <div>
+            <Keyboard highlightKeys={highlightKeys} />
+        </div>
+    );
 }
 
+const MinorTriad: Record<string,string[]> = {
+    C: ["C4", "D#4", "G4"],
+    D: ["D4", "F4", "A5"],
+    E: ["E4", "F#4", "B5"],
+    F: ["F4", "G#", "C5"],
+    G: ["G4", "A", "D5"],
+    A: ["A5", "C", "E5"],
+    B: ["B5", "C#", "F5"]
+};
 
-
-export default;
+export default HeaderQuiz;
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,6 +1,6 @@
 import "./Keyboard.css"
 
-function Keyboard() {
+function Keyboard({ highlightKeys }: { highlightKeys: string[] }) {
     const notes: string[] = [
         "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A5", "A#5", "B5",
         "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A6", "A#6", "B5"
