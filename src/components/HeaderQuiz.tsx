bc278045169ea997466d178833054b36d0976206
cy26015 <cy26015@shibaura-it.ac.jp>
2026-09-01 16:05:18 +0900

    majorTriad

---
+++ src/components/HeaderQuiz.tsx
@@ -0,0 +1,20 @@
+import Keyboard from "./Keyboard";
+
+/*
+"C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A5", "A#5", "B5",
+"C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A6", "A#6", "B5"
+*/
+
+const majorTriads: Record<string, string>[] => {
+    C: ["C4", "E4", "G4"]
+    D: ["D4", "F#4", "A5"]
+    E: ["E4", "G4", "B5"]
+    F: ["F4", "A5", "C5"]
+    G: ["G4", "B5", "D5"]
+    A: ["A5", "C#5", "E5"]
+    B: ["B5", "D5", "F5"]
+}
+
+
+
+export default;
